c142f7695ac438b345662436945c64cb3cfa4307
Benjamin Kearns <rapiidz1@gmail.com>
2018-11-26 16:16:55 +0000

    working on hello world

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Learning Java
 ------
-A repository I will use to help me learn java.
+A repository I will use to help me learn Java.
---
+++ helloworld.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+  public static void main(String[] args) {};
+  public void sayHello(String name) {
+    System.out.println("Hello World!" + name);
+  };
+};
